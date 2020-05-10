04b465d5cff93a59aa78c674f1abe9c2266c0e0c
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-05-10 19:03:13 +0900

    Extending the Schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,9 @@
+import { people, getById } from "./db"
+
 const resolvers = {
     Query: {
-        name: () => "jeongseop"
+        people: () => people,
+        person: (_, { id }) => getById(id)
     }
 };
 
