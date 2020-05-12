ad156aba3e2c57d91e99861711a99e33d31595ce
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-05-12 18:09:09 +0900

    add getMovie

--- graphql/db.js
+++ graphql/db.js
@@ -1,9 +1,10 @@
 import fetch from "node-fetch";
 
-const API_URL = "https://yts.mx/api/v2/list_movies.json?";
+const API_URL_LIST = "https://yts.mx/api/v2/list_movies.json?";
+const API_URL_DETAIL = "https://yts.mx/api/v2/movie_details.json?"
 
 export const getMovies = async (limit, rating) => {
-    let REQUEST_URL = API_URL;
+    let REQUEST_URL = API_URL_LIST;
     if(limit > 0){
         REQUEST_URL += `&limit=${limit}`;
     }
@@ -14,3 +15,10 @@ export const getMovies = async (limit, rating) => {
     const json = await res.json();
     return json.data.movies;
 }
+
+export const getMovie = async (id) => {
+    const res = await fetch(API_URL_DETAIL+`movie_id=${id}`);
+    const json = await res.json();
+    return json.data.movie;
+}
+
