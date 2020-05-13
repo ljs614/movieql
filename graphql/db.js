9842ed6c7fc78a9c30e41f3693574157c550ad02
jeongseop.lee <jeongseop.lee@wizontech.com>
2020-05-13 11:35:20 +0900

    Add suggestions, Use axios instead of fetch

--- graphql/db.js
+++ graphql/db.js
@@ -1,24 +1,47 @@
-import fetch from "node-fetch";
+import axios from "axios";
 
-const API_URL_LIST = "https://yts.mx/api/v2/list_movies.json?";
-const API_URL_DETAIL = "https://yts.mx/api/v2/movie_details.json?"
+const BASE_URL = "https://yts.mx/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAIL_URL = `${BASE_URL}list_movies.jsonmovie_details.json`;
+const MOVIES_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
 export const getMovies = async (limit, rating) => {
-    let REQUEST_URL = API_URL_LIST;
-    if(limit > 0){
-        REQUEST_URL += `&limit=${limit}`;
-    }
-    if(rating > 0){
-        REQUEST_URL += `&minimum_rating=${rating}`;
-    }
-    const res = await fetch(REQUEST_URL);
-    const json = await res.json();
-    return json.data.movies;
+    const {
+        data: {
+            data: { movies }
+        }
+    } = await axios(LIST_MOVIES_URL, {
+        params: {
+            limit,
+            minimum_rating: rating
+        }
+    });
+    return movies;
 }
 
 export const getMovie = async (id) => {
-    const res = await fetch(API_URL_DETAIL+`movie_id=${id}`);
-    const json = await res.json();
-    return json.data.movie;
+    const {
+        data: {
+            data: { movie }
+        }
+    } = await axios(MOVIE_DETAIL_URL, {
+        params: {
+            movie_id: id
+        }
+    });
+    return movie;
+}
+
+export const getSuggestions = async id => {
+    const {
+        data: {
+            data: { movies }
+        }
+    } = await axios(MOVIES_SUGGESTIONS_URL, {
+        params: {
+            movie_id: id
+        }
+    });
+    return movies;
 }
 
